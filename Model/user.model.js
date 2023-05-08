8daa8c502912fcb6b2c5db575c4c632a26451343
Kunjan Ballawar <kballawar0@gmail.com>
2023-05-08 12:37:04 +0530

    JJ

--- Model/user.model.js
+++ Model/user.model.js
@@ -5,7 +5,7 @@ const userSchema = Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  age: { type: Number, required: true },
+  age: { type: Number },
 },
 {
   versionKey: false,
